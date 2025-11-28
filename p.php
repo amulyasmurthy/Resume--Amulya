f8ad15495dec27e25e486d01cc294b7ec10db520
amulyasmurthy <amulyasmurthy513@gmail.com>
2025-11-28 06:53:55 +0530

    Add files via upload

---
+++ p.php
@@ -0,0 +1,10 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    echo "<h2>Thank you, $name!</h2>";
+    echo "<p>Your message has been received.</p>";
+}
+?>
